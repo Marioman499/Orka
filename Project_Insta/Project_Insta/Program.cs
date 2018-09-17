fee95a053aba64d58906e1e4dfbdb4167e8f7a8f
Hayden <HCButler@student.fullsail.edu>
2018-09-17 15:21:05 -0400

    adding project insta to repo

---
+++ Project_Insta/Project_Insta/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Project_Insta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebElement username;
+            IWebElement password;
+            
+
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.instagram.com/accounts/login/?source=auth_switcher");
+            for (int i = 0; i < 10; i++)
+            {
+                
+               
+                IWebElement element = driver.FindElement(By.TagName("body"));
+                element.SendKeys(OpenQA.Selenium.Keys.Control + "t");
+
+            }
+
+
+
+
+        }
+    }
+}
