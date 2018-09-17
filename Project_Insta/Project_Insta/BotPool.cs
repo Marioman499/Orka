77cde75abf856ba9da84239d203d825a89b87287
Hayden <HCButler@student.fullsail.edu>
2018-09-17 17:03:54 -0400

    added bot pool class and added basic functions

---
+++ Project_Insta/Project_Insta/BotPool.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Insta
+{
+    class BotPool
+    {
+        private List<Bot> Bot_Pool;
+
+        public bool SearchPool(Bot _bot)
+        {
+            for (int i = 0; i < Bot_Pool.Capacity; i++)
+            {
+                if (_bot.GetId() == Bot_Pool[i].GetId())
+                {
+                    return true;
+                }
+                else
+                {
+
+                }
+            }
+            return false;
+        }
+        public void Generate()
+        {
+
+        }
+    }
+}
